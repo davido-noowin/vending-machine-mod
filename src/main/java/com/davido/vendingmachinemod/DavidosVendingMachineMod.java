f212790a9cd6de91ce1a815d8c88645371bf553e
David Nguyen <davido.nguyen1212@gmail.com>
2025-12-16 22:29:39 -0800

    add 2 drinks to the mod

--- src/main/java/com/davido/vendingmachinemod/DavidosVendingMachineMod.java
+++ src/main/java/com/davido/vendingmachinemod/DavidosVendingMachineMod.java
@@ -1,5 +1,6 @@
 package com.davido.vendingmachinemod;
 
+import com.davido.vendingmachinemod.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -54,6 +55,8 @@ public class DavidosVendingMachineMod {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -67,7 +70,10 @@ public class DavidosVendingMachineMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.CANNED_DRINK);
+            event.accept(ModItems.SENZEE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
--- src/main/java/com/davido/vendingmachinemod/item/ModItems.java
+++ src/main/java/com/davido/vendingmachinemod/item/ModItems.java
@@ -1,4 +1,21 @@
 package com.davido.vendingmachinemod.item;
 
+import com.davido.vendingmachinemod.DavidosVendingMachineMod;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
 public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(DavidosVendingMachineMod.MODID);
+
+    public static final DeferredItem<Item> CANNED_DRINK = ITEMS.register("emptydrink",
+            () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> SENZEE = ITEMS.register("senzee",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
